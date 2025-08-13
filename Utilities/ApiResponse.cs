3d61a794e37fda81f78273f46e1254f1a2aadb6c
Noelly Ruiz Correa <noelly.ruizcor@ug.edu.ec>
2025-08-13 12:35:39 -0500

    agregue mi clase generica

---
+++ Utilities/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Utilities
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+
+        public string? Message { get; set; }
+
+        public T? Data { get; set; }
+    }
+}
