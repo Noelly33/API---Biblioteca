8dee0d487a6b6b82574cb534e37833e7a6ddef64
Noelly Ruiz Correa <noelly.ruizcor@ug.edu.ec>
2025-08-13 23:43:57 -0500

    Subiendo nuevas interfaces en repository de DataBaseFirst

---
+++ DataBaseFirst/Repository/UsuarioRepository.cs
@@ -0,0 +1,60 @@
+﻿using DataBaseFirst.Data;
+using DataBaseFirst.Models;
+using DataBaseFirst.Repository.InterfacesRepository;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseFirst.Repository
+{
+    public class UsuarioRepository : IUsuario
+    {
+        private readonly DBContext _dbContext; 
+
+        public UsuarioRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Usuario>> ListaUsuarios()
+        {
+            return await _dbContext.Usuarios.FromSqlRaw("EXEC PA_LISTAR_USUARIOS").ToListAsync();
+        }
+
+        public async Task<Usuario> ObtenerUsuarioNombre(string nombres)
+        {
+            var id = new SqlParameter("@Nombres", nombres);
+            return await Task.Run(() => _dbContext.Usuarios.FromSqlRaw("EXEC PA_BUSCAR_NOMBRE_USUARIO @nombres", id).AsNoTracking().AsEnumerable().FirstOrDefault());
+        }
+
+        public async Task<int> RegistrarUsuario(Usuario usuario)
+        {
+            return await _dbContext.Database.ExecuteSqlRawAsync("EXEC PA_REGISTRAR_USUARIO @Nombres, @Apellidos, @Correo, @Estado",
+
+                new SqlParameter("@Nombres", usuario.Nombres ?? (object)DBNull.Value),
+                new SqlParameter("@Apellidos", usuario.Apellidos ?? (object)DBNull.Value),
+                new SqlParameter("@Correo", usuario.Correo ?? (object)DBNull.Value),
+                new SqlParameter("@Estado", usuario.Estado ?? false)
+                );
+        }
+
+        public async Task<int> EditarUsuario(Usuario usuario)
+        {
+            return await _dbContext.Database.ExecuteSqlRawAsync("EXEC PA_EDITAR_USUARIO @Id_Usuario, @Nombres, @Apellidos, @Correo, @Estado",
+
+                new SqlParameter("@Id_Usuario", usuario.IdUsuario),
+                new SqlParameter("@Nombres", usuario.Nombres ?? (object)DBNull.Value),
+                new SqlParameter("@Apellidos", usuario.Apellidos ?? (object)DBNull.Value),
+                new SqlParameter("@Correo", usuario.Correo ?? (object)DBNull.Value),
+                new SqlParameter("@Estado", usuario.Estado ?? false)
+                );
+        }
+
+        public async Task<int> EliminarUsuario(int id)
+        {
+            return await _dbContext.Database.ExecuteSqlRawAsync("EXEC PA_ELIMINAR_USUARIO @Id_Usuario",
+                new SqlParameter("@Id_Usuario", id)
+                );
+        }
+
+    }
+}
