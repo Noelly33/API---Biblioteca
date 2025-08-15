b60388964d30ea92c5442d021bd89a86b7ebff65
Noelly Ruiz Correa <noelly.ruizcor@ug.edu.ec>
2025-08-15 01:07:51 -0500

    Agregue nueva interfaceService

---
+++ DataBaseFirst/Repository/InterfacesService/IAutorService.cs
@@ -0,0 +1,20 @@
+﻿using DataBaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace DataBaseFirst.Repository.InterfacesService
+{
+    public interface IAutorService
+    {
+        Task<ApiResponse<List<Autor>>> ListaAutores();
+        Task<ApiResponse<Autor>> ObtenerAutorNombre(string nombre);
+        Task<ApiResponse<Autor>> ObtenerAutorID(int id);
+        Task<ApiResponse<object>> RegistrarAutor(Autor autor);
+        Task<ApiResponse<object>> EditarAutor(Autor autor);
+        Task<ApiResponse<int>> EliminarAutor(int id);
+    }
+}
---
+++ DataBaseFirst/Repository/InterfacesService/IPrestamoService.cs
@@ -0,0 +1,18 @@
+﻿using DataBaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace DataBaseFirst.Repository.InterfacesService
+{
+    public interface IPrestamoService
+    {
+        Task<ApiResponse<List<Prestamo>>> ListaPrestamos();
+        Task<ApiResponse<Prestamo>> ObtenerPorUsuario(int id);
+        Task<ApiResponse<Prestamo>> ObtenerPorLibro(int id);
+        Task<ApiResponse<object>> RegistrarPrestamo(Prestamo prestamo);
+    }
+}
