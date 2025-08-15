08cbdf9b56b0cee11b4a52d6bb8d000f27db8422
Noelly Ruiz Correa <noelly.ruizcor@ug.edu.ec>
2025-08-15 01:08:29 -0500

    agregue un nuevo service

---
+++ DataBaseFirst/Service/AutorService.cs
@@ -0,0 +1,110 @@
+﻿using DataBaseFirst.Data;
+using DataBaseFirst.Models;
+using DataBaseFirst.Repository;
+using DataBaseFirst.Repository.InterfacesRepository;
+using DataBaseFirst.Repository.InterfacesService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace DataBaseFirst.Service
+{
+    public class AutorService : IAutorService
+    {
+        public readonly AutorRepository _autorRepository;
+
+        public AutorService(AutorRepository autorRepository) 
+        { 
+        
+            _autorRepository = autorRepository;
+        }
+
+        public async Task<ApiResponse<List<Autor>>> ListaAutores()
+        {
+            var listaAutores = await _autorRepository.ListaAutores();
+
+            if(listaAutores == null)
+                return new ApiResponse<List<Autor>> { IsSuccess = false, Message = Mensajes.MESSAGE_QUERY_EMPTY , Data = listaAutores };
+
+            return new ApiResponse<List<Autor>> { IsSuccess = true, Message = Mensajes.MESSAGE_QUERY_SUCCESS, Data = listaAutores };
+
+        }
+
+        public async Task<ApiResponse<Autor>> ObtenerAutorNombre(string nombre)
+        {
+            var nombreAutor = await _autorRepository.ObtenerAutorNombre(nombre);
+
+            if(nombre == null)
+                return new ApiResponse<Autor> { IsSuccess = false , Message = Mensajes.MESSAGE_QUERY_EMPTY, Data = nombreAutor };
+
+            return new ApiResponse<Autor> { IsSuccess = true , Message = Mensajes.MESSAGE_QUERY_SUCCESS, Data=nombreAutor };
+        }
+
+        public async Task<ApiResponse<Autor>> ObtenerAutorID(int id)
+        {
+            var idAutor = await _autorRepository.ObtenerAutorID(id);
+
+            if (idAutor == null)
+                return new ApiResponse<Autor> { IsSuccess = false, Message = Mensajes.MESSAGE_QUERY_EMPTY, Data = idAutor };
+
+            return new ApiResponse<Autor> { IsSuccess = true, Message = Mensajes.MESSAGE_QUERY_SUCCESS, Data = idAutor };
+
+        }
+
+        public async Task<ApiResponse<object>> RegistrarAutor(Autor autor)
+        {
+            if(autor == null)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = autor };
+
+            if(string.IsNullOrWhiteSpace(autor.Nombre) || string.IsNullOrWhiteSpace(autor.Nacionalidad))
+                return new ApiResponse<object> { IsSuccess = false , Message = Mensajes.MESSAGE_ERROR,Data = autor };
+
+            var validacion = new Regex("^[a-zA-ZáéíóúÁÉÍÓÚñN]+ $");
+            if(!validacion.IsMatch(autor.Nombre) || !validacion.IsMatch(autor.Nacionalidad))
+                return new ApiResponse<object> {IsSuccess = false , Message=Mensajes.MESSAGE_ERROR, Data=autor };
+
+            var resultado = await _autorRepository.RegistrarAutor(autor);
+            if (resultado > 0)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR };
+
+            return new ApiResponse<object> {IsSuccess = true, Message = Mensajes.MESSAGE_REGISTER, Data=autor};
+        }
+
+        public async Task<ApiResponse<object>> EditarAutor(Autor autor)
+        {
+            var validacionPrincipal = await _autorRepository.ObtenerAutorID(autor.IdAutor);
+            if (validacionPrincipal == null)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR };
+
+            if (autor == null)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = autor };
+
+            if (string.IsNullOrWhiteSpace(autor.Nombre) || string.IsNullOrWhiteSpace(autor.Nacionalidad))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = autor };
+
+            var validacion = new Regex("^[a-zA-ZáéíóúÁÉÍÓÚñN]+ $");
+            if (!validacion.IsMatch(autor.Nombre) || !validacion.IsMatch(autor.Nacionalidad))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = autor };
+
+            var resultado = await _autorRepository.EditarAutor(autor);
+            if (resultado > 0)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR };
+
+            return new ApiResponse<object> { IsSuccess = true, Message = Mensajes.MESSAGE_UPDATE, Data = autor };
+        }
+
+        public async Task<ApiResponse<int>> EliminarAutor(int idAutor)
+        {
+            var autor = await _autorRepository.ObtenerAutorID(idAutor);
+            if(autor == null)
+                return new ApiResponse<int> { IsSuccess = false, Message= Mensajes.MESSAGE_ERROR };
+
+            return new ApiResponse<int> { IsSuccess = true, Message= Mensajes.MESSAGE_DELETE };
+        }
+
+    }
+}
---
+++ DataBaseFirst/Service/UsuarioService.cs
@@ -0,0 +1,116 @@
+﻿using DataBaseFirst.Models;
+using DataBaseFirst.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace DataBaseFirst.Service
+{
+    public class UsuarioService
+    {
+
+        public readonly UsuarioRepository _usuarioRepository;
+
+        public UsuarioService(UsuarioRepository usuarioRepository)
+        {
+
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public async Task<ApiResponse<List<Usuario>>> ListaUsuarios()
+        {
+            var listaUsuarios = await _usuarioRepository.ListaUsuarios();
+
+            if (listaUsuarios == null)
+                return new ApiResponse<List<Usuario>> { IsSuccess = false, Message = Mensajes.MESSAGE_QUERY_EMPTY, Data = listaUsuarios };
+
+            return new ApiResponse<List<Usuario>> { IsSuccess = true, Message = Mensajes.MESSAGE_QUERY_SUCCESS, Data = listaUsuarios };
+
+        }
+
+        public async Task<ApiResponse<Usuario>> ObtenerUsuarioNombre(string nombres)
+        {
+            var nombreUsuario = await _usuarioRepository.ObtenerUsuarioNombre(nombres);
+
+            if (nombres == null)
+                return new ApiResponse<Usuario> { IsSuccess = false, Message = Mensajes.MESSAGE_QUERY_EMPTY, Data = nombreUsuario };
+
+            return new ApiResponse<Usuario> { IsSuccess = true, Message = Mensajes.MESSAGE_QUERY_SUCCESS, Data = nombreUsuario };
+        }
+
+        public async Task<ApiResponse<Usuario>> ObtenerUsuarioID(int id)
+        {
+            var idUsuario = await _usuarioRepository.ObtenerUsuarioID(id);
+
+            if (idUsuario == null)
+                return new ApiResponse<Usuario> { IsSuccess = false, Message = Mensajes.MESSAGE_QUERY_EMPTY, Data = idUsuario };
+
+            return new ApiResponse<Usuario> { IsSuccess = true, Message = Mensajes.MESSAGE_QUERY_SUCCESS, Data = idUsuario };
+
+        }
+
+        public async Task<ApiResponse<object>> RegistrarUsuario(Usuario usuario)
+        {
+            if (usuario == null)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            if (string.IsNullOrWhiteSpace(usuario.Nombres) || string.IsNullOrWhiteSpace(usuario.Apellidos) || string.IsNullOrWhiteSpace(usuario.Correo))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            var validacion = new Regex("^[a-zA-ZáéíóúÁÉÍÓÚñN]+ $");
+            if (!validacion.IsMatch(usuario.Nombres) || !validacion.IsMatch(usuario.Apellidos))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            var validacion2 = new Regex(@"^[@\s]+@[^@\s]+\.[^@\s]+$");
+            if (!validacion.IsMatch(usuario.Correo))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            var resultado = await _usuarioRepository.RegistrarUsuario(usuario);
+            if (resultado > 0)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR };
+
+            return new ApiResponse<object> { IsSuccess = true, Message = Mensajes.MESSAGE_REGISTER, Data = usuario };
+        }
+
+        public async Task<ApiResponse<object>> EditarUsuario(Usuario usuario)
+        {
+            var validacionPrincipal = await _usuarioRepository.ObtenerUsuarioID(usuario.IdUsuario);
+            if (validacionPrincipal == null)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR };
+
+            if(usuario == null)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            if (string.IsNullOrWhiteSpace(usuario.Nombres) || string.IsNullOrWhiteSpace(usuario.Apellidos) || string.IsNullOrWhiteSpace(usuario.Correo))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            var validacion = new Regex("^[a-zA-ZáéíóúÁÉÍÓÚñN]+ $");
+            if (!validacion.IsMatch(usuario.Nombres) || !validacion.IsMatch(usuario.Apellidos))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            var validacion2 = new Regex(@"^[@\s]+@[^@\s]+\.[^@\s]+$");
+            if (!validacion.IsMatch(usuario.Correo))
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR, Data = usuario };
+
+            var resultado = await _usuarioRepository.EditarUsuario(usuario);
+            if (resultado > 0)
+                return new ApiResponse<object> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR };
+
+            return new ApiResponse<object> { IsSuccess = true, Message = Mensajes.MESSAGE_UPDATE, Data = usuario };
+        }
+
+        public async Task<ApiResponse<int>> EliminarUsuario(int idUsuario)
+        {
+            var usuario = await _usuarioRepository.ObtenerUsuarioID(idUsuario);
+            if (usuario == null)
+                return new ApiResponse<int> { IsSuccess = false, Message = Mensajes.MESSAGE_ERROR };
+
+            return new ApiResponse<int> { IsSuccess = true, Message = Mensajes.MESSAGE_DELETE };
+        }
+
+    }
+}
