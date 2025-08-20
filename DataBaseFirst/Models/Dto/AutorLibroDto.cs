e2324f088d19cf8a88e6b7ed610401c769d8c6a4
Noelly Ruiz Correa <noelly.ruizcor@ug.edu.ec>
2025-08-20 11:07:35 -0500

    Agregue Dtos en models

---
+++ DataBaseFirst/Models/Dto/AutorLibroDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirst.Models.Dto
+{
+    public class AutorLibroDto
+    {
+        public string? Nombre { get; set; }
+        public string? Titulo { get; set; }
+    }
+}
---
+++ DataBaseFirst/Models/Dto/LibroPrestamoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirst.Models.Dto
+{
+    public class LibroPrestamoDto
+    {
+        public string? Titulo { get; set; }
+        public string ? Nombres { get; set; }
+        public DateTime Fecha_Prestamo { get; set; }
+    }
+}
