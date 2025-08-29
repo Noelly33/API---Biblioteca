72b647b1b40484eb8d8da52d0ecffee69fe71c07
Noelly Ruiz Correa <noelly.ruizcor@ug.edu.ec>
2025-08-29 00:28:18 -0500

    Agregue metodos en interface Libro

--- DataBaseFirst/Repository/InterfacesService/ILibroService.cs
+++ DataBaseFirst/Repository/InterfacesService/ILibroService.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Utilities;
+using Utilities.Shared;
 
 namespace DataBaseFirst.Repository.InterfacesService
 {
